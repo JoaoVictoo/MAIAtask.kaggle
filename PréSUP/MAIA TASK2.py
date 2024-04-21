94879ee62ab245d3d8b9722fcdec42639b4e1bce
JoaoVictoo <palmeirajoao08@gmail.com>
2024-04-20 21:35:37 -0300

    firt commit

---
+++ PréSUP/MAIA TASK2.py
@@ -0,0 +1,7 @@
+import pandas as pd
+import numpy as np
+
+train = pd.read_csv('C:/Users/Satoshii/Desktop/All/Titanic/PréSUP/trainSUP')
+test = pd.read_csv('C:/Users/Satoshii/Desktop/All/Titanic/PréSUP/test')
+
+test.head() 
